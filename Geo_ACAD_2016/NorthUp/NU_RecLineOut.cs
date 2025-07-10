63feb6f34befc4cbe315fec62cac9ea30cc0f6ec
Fung W.F. Lee <funglee@egssurvey.com>
2025-07-10 09:46:03 +0800

    Fix: detecting incorrect -ve sample rate from source without sample# (-999)

--- Geo_ACAD_2016/NorthUp/NU_RecLineOut.cs
+++ Geo_ACAD_2016/NorthUp/NU_RecLineOut.cs
@@ -4,40 +4,48 @@ using static Geo_AC2016.SharedFunctions;
 
 namespace Geo_AC2016
 {
-    internal class NU_RecLineOut
-    {
-        internal static string RecLine_Out(RF iRF, int vel_sedi, int vel_water, int sample_rate)
-        {
-            List<string> recout = new List<string>();
-            double SeabedSN, RefSN;
-            switch (iRF.otype)
-            {
-                case 1:
-                    recout.Add("START REFLECTOR");
-                    foreach (XYZZ iXYZZ in iRF.xyzz)
-                    {
-                        SeabedSN = (iXYZZ.ZTxd - iXYZZ.ZSb) * 2 / vel_water * sample_rate;
-                        RefSN = SeabedSN + iXYZZ.ZSb * 2 / vel_sedi * sample_rate;
-                        recout.Add(String.Format("RecNo {0,8:0} RefSampleNo {1,6:0} SeabedSampleNo {2,6:0} Pos {3,12:0.000} E {4,12:0.000} N ZTxd {5,6:0.00} ZSb {6,6:0.00}",
-                            iXYZZ.Rec, RefSN, SeabedSN,
-                            iXYZZ.X, iXYZZ.Y, iXYZZ.ZTxd, iXYZZ.ZSb));
-                    }
-                    break;
+	internal class NU_RecLineOut
+	{
+		internal static string RecLine_Out(RF iRF, int vel_sedi, int vel_water, int sample_rate)
+		{
+			List<string> recout = new List<string>();
+			double SeabedSN, RefSN;
+			switch (iRF.otype)
+			{
+				case 1:
+					recout.Add("START REFLECTOR");
+					foreach (XYZZ iXYZZ in iRF.xyzz)
+					{
+						if (sample_rate > 0)
+						{
+							SeabedSN = (iXYZZ.ZTxd - iXYZZ.ZSb) * 2 / vel_water * sample_rate;
+							RefSN = SeabedSN + iXYZZ.ZSb * 2 / vel_sedi * sample_rate;
+						}
+						else { SeabedSN = -999; RefSN = -999; }
+						recout.Add(String.Format("RecNo {0,8:0} RefSampleNo {1,6:0} SeabedSampleNo {2,6:0} Pos {3,12:0.000} E {4,12:0.000} N ZTxd {5,6:0.00} ZSb {6,6:0.00}",
+							 iXYZZ.Rec, RefSN, SeabedSN,
+							 iXYZZ.X, iXYZZ.Y, iXYZZ.ZTxd, iXYZZ.ZSb));
+					}
+					break;
 
-                case 2:
-                    recout.Add("ANNOTATION RECORD");
-                    SeabedSN = (iRF.xyzz[0].ZTxd - iRF.xyzz[0].ZSb) * 2 / vel_water * sample_rate;
-                    RefSN = SeabedSN + iRF.xyzz[0].ZSb * 2 / vel_sedi * sample_rate;
-                    recout.Add(String.Format("RecNo {0,8:0} RefSampleNo {1,6:0} SeabedSampleNo {2,6:0} Pos {3,12:0.000} E {4,12:0.000} N ZTxd {5,6:0.00} ZSb {6,6:0.00} Annotation {7,40}",
-                        iRF.xyzz[0].Rec, RefSN, SeabedSN,
-                        iRF.xyzz[0].X, iRF.xyzz[0].Y, iRF.xyzz[0].ZTxd, iRF.xyzz[0].ZSb, iRF.annotation));
-                    break;
+				case 2:
+					recout.Add("ANNOTATION RECORD");
+					if (sample_rate > 0)
+					{
+						SeabedSN = (iRF.xyzz[0].ZTxd - iRF.xyzz[0].ZSb) * 2 / vel_water * sample_rate;
+						RefSN = SeabedSN + iRF.xyzz[0].ZSb * 2 / vel_sedi * sample_rate;
+					}
+					else { SeabedSN = -999; RefSN = -999; }
+					recout.Add(String.Format("RecNo {0,8:0} RefSampleNo {1,6:0} SeabedSampleNo {2,6:0} Pos {3,12:0.000} E {4,12:0.000} N ZTxd {5,6:0.00} ZSb {6,6:0.00} Annotation {7,40}",
+						 iRF.xyzz[0].Rec, RefSN, SeabedSN,
+						 iRF.xyzz[0].X, iRF.xyzz[0].Y, iRF.xyzz[0].ZTxd, iRF.xyzz[0].ZSb, iRF.annotation));
+					break;
 
-                default:
-                    recout.Clear();
-                    break;
-            }
-            return string.Join("\r\n", recout.ToArray());
-        }
-    }
+				default:
+					recout.Clear();
+					break;
+			}
+			return string.Join("\r\n", recout.ToArray());
+		}
+	}
 }
