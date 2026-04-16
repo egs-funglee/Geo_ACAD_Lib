a059291c0fa4ca354ce1d041c3b2abe0c5988293
Fung W.F. Lee <funglee@egssurvey.com>
2026-04-16 19:01:52 +0800

    update to use text height from in ini file

--- Geo_ACAD_2016/Profile/SBP_Plot_Fix_from_CSV.cs
+++ Geo_ACAD_2016/Profile/SBP_Plot_Fix_from_CSV.cs
@@ -2,121 +2,128 @@
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
+using static Geo_AC2016.SharedFunctions;
 
 namespace Geo_AC2016
 {
-    internal class SBP_Plot_Fix_from_CSV
-    {
-        internal static void Plot_Fix_from_CSV_Profile()
-        {
-            Document adoc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
-            Editor ed = adoc.Editor;
-            Database db = adoc.Database;
+	internal class SBP_Plot_Fix_from_CSV
+	{
+		internal static void Plot_Fix_from_CSV_Profile()
+		{
+			Document adoc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
+			Editor ed = adoc.Editor;
+			Database db = adoc.Database;
 
-            string fixcsv = @"C:\EGS\SBP_Fix.txt";
+			string fixcsv = @"C:\EGS\SBP_Fix.txt";
 
-            if (!System.IO.File.Exists(fixcsv))
-            {
-                ed.WriteMessage($"\n{fixcsv} is not exists.\n");
-                return;
-            }
+			if (!System.IO.File.Exists(fixcsv))
+			{
+				ed.WriteMessage($"\n{fixcsv} is not exists.\n");
+				return;
+			}
 
-            string[] fix = System.IO.File.ReadAllLines(fixcsv);
-            if (fix.Length == 0)
-            {
-                ed.WriteMessage($"\n{fixcsv} is empty.\n");
-                return;
-            }
+			string[] fix = System.IO.File.ReadAllLines(fixcsv);
+			if (fix.Length == 0)
+			{
+				ed.WriteMessage($"\n{fixcsv} is empty.\n");
+				return;
+			}
 
-            string[] tempstr;
-            DBText acText;
-            Polyline acPoly;
-            double gridx;
-            string en;
+			string[] tempstr;
+			DBText acText;
+			Polyline acPoly;
+			double gridx;
+			string en;
 
-            using (Transaction tr = db.TransactionManager.StartTransaction())
-            {
-                BlockTable acBlkTbl = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
-                BlockTableRecord acBlkTblRec = tr.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+			//set font height
+			IniParser.Model.IniData ini = Read_ini();
+			string str_pKeyRes = ini["Parameters"]["Text Height"];
+			if (string.IsNullOrEmpty(str_pKeyRes)) str_pKeyRes = "20";
+			double acad_textheight = double.Parse(str_pKeyRes);
 
-                LayerTable acLayerTbl = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForRead);
-                TextStyleTable textStyles = (TextStyleTable)tr.GetObject(db.TextStyleTableId, OpenMode.ForRead);
+			using (Transaction tr = db.TransactionManager.StartTransaction())
+			{
+				BlockTable acBlkTbl = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+				BlockTableRecord acBlkTblRec = tr.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                if (!textStyles.Has("Arial75"))
-                {
-                    textStyles.UpgradeOpen();
-                    var newTextStyleTableRecord = new TextStyleTableRecord
-                    {
-                        Name = "Arial75",
-                        XScale = 0.75,
-                        TextSize = 20,
-                        Font = new Autodesk.AutoCAD.GraphicsInterface.FontDescriptor("Arial", false, false, 0, 0)
-                    };
-                    textStyles.Add(newTextStyleTableRecord);
-                    tr.AddNewlyCreatedDBObject(newTextStyleTableRecord, true);
-                    db.Textstyle = newTextStyleTableRecord.ObjectId;
-                    textStyles.DowngradeOpen();
-                }
-                else
-                    db.Textstyle = textStyles["Arial75"];
+				LayerTable acLayerTbl = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForRead);
+				TextStyleTable textStyles = (TextStyleTable)tr.GetObject(db.TextStyleTableId, OpenMode.ForRead);
 
-                string lastlayer = "0";
-                ObjectId activelayer = db.Clayer;
+				if (!textStyles.Has("Arial75"))
+				{
+					textStyles.UpgradeOpen();
+					var newTextStyleTableRecord = new TextStyleTableRecord
+					{
+						Name = "Arial75",
+						XScale = 0.75,
+						TextSize = acad_textheight,
+						Font = new Autodesk.AutoCAD.GraphicsInterface.FontDescriptor("Arial", false, false, 0, 0)
+					};
+					textStyles.Add(newTextStyleTableRecord);
+					tr.AddNewlyCreatedDBObject(newTextStyleTableRecord, true);
+					db.Textstyle = newTextStyleTableRecord.ObjectId;
+					textStyles.DowngradeOpen();
+				}
+				else
+					db.Textstyle = textStyles["Arial75"];
 
-                foreach (string sline in fix)
-                {
-                    tempstr = sline.Split(','); // GridX0, Fix1, E2, N3, Layer4
+				string lastlayer = "0";
+				ObjectId activelayer = db.Clayer;
 
-                    if (tempstr[4] != lastlayer)
-                    {
-                        lastlayer = tempstr[4];
-                        if (!acLayerTbl.Has(tempstr[4]))
-                        {
-                            acLayerTbl.UpgradeOpen();
-                            LayerTableRecord newLayer = new LayerTableRecord { Name = tempstr[4] };
-                            acLayerTbl.Add(newLayer);
-                            tr.AddNewlyCreatedDBObject(newLayer, true);
-                            acLayerTbl.DowngradeOpen();
-                        }
-                    }
+				foreach (string sline in fix)
+				{
+					tempstr = sline.Split(','); // GridX0, Fix1, E2, N3, Layer4
 
-                    if (tempstr[4] != "0")
-                        db.Clayer = acLayerTbl[tempstr[4]];
+					if (tempstr[4] != lastlayer)
+					{
+						lastlayer = tempstr[4];
+						if (!acLayerTbl.Has(tempstr[4]))
+						{
+							acLayerTbl.UpgradeOpen();
+							LayerTableRecord newLayer = new LayerTableRecord { Name = tempstr[4] };
+							acLayerTbl.Add(newLayer);
+							tr.AddNewlyCreatedDBObject(newLayer, true);
+							acLayerTbl.DowngradeOpen();
+						}
+					}
 
-                    gridx = double.Parse(tempstr[0]);
-                    en = tempstr[2] + "," + tempstr[3];
+					if (tempstr[4] != "0")
+						db.Clayer = acLayerTbl[tempstr[4]];
 
-                    HyperLink hyper = new HyperLink
-                    {
-                        Description = en,
-                        Name = en,
-                        SubLocation = ""
-                    };
+					gridx = double.Parse(tempstr[0]);
+					en = tempstr[2] + "," + tempstr[3];
 
-                    acPoly = new Polyline();
-                    acPoly.SetDatabaseDefaults();
-                    acPoly.AddVertexAt(0, new Point2d(gridx, 0), 0, 0, 0);
-                    acPoly.AddVertexAt(1, new Point2d(gridx, 393.7), 0, 0, 0);
-                    acPoly.Color = Autodesk.AutoCAD.Colors.Color.FromColorIndex(Autodesk.AutoCAD.Colors.ColorMethod.ByAci, 214);
+					HyperLink hyper = new HyperLink
+					{
+						Description = en,
+						Name = en,
+						SubLocation = ""
+					};
 
-                    acText = new DBText();
-                    acText.SetDatabaseDefaults();
-                    acText.Justify = AttachmentPoint.MiddleCenter;
-                    acText.AlignmentPoint = new Point3d(gridx, 437.89417382, 0);
-                    acText.Height = 20;
-                    acText.TextString = tempstr[1];
+					acPoly = new Polyline();
+					acPoly.SetDatabaseDefaults();
+					acPoly.AddVertexAt(0, new Point2d(gridx, 0), 0, 0, 0);
+					acPoly.AddVertexAt(1, new Point2d(gridx, 393.7), 0, 0, 0);
+					acPoly.Color = Autodesk.AutoCAD.Colors.Color.FromColorIndex(Autodesk.AutoCAD.Colors.ColorMethod.ByAci, 214);
 
-                    acBlkTblRec.AppendEntity(acText);
-                    tr.AddNewlyCreatedDBObject(acText, true);
-                    acText.Hyperlinks.Add(hyper);
+					acText = new DBText();
+					acText.SetDatabaseDefaults();
+					acText.Justify = AttachmentPoint.MiddleCenter;
+					acText.AlignmentPoint = new Point3d(gridx, 437.89417382, 0);
+					acText.Height = acad_textheight;
+					acText.TextString = tempstr[1];
 
-                    acBlkTblRec.AppendEntity(acPoly);
-                    tr.AddNewlyCreatedDBObject(acPoly, true);
-                }
+					acBlkTblRec.AppendEntity(acText);
+					tr.AddNewlyCreatedDBObject(acText, true);
+					acText.Hyperlinks.Add(hyper);
 
-                tr.Commit();
-                db.Clayer = activelayer;
-            }
-        }
-    }
+					acBlkTblRec.AppendEntity(acPoly);
+					tr.AddNewlyCreatedDBObject(acPoly, true);
+				}
+
+				tr.Commit();
+				db.Clayer = activelayer;
+			}
+		}
+	}
 }
